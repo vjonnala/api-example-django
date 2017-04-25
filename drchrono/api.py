cec60d665fd33937fc37d72a676781d2c3045689
Joseph Dougherty <jdougherty@jwdougherty.com>
2017-04-24 20:45:57 -0400

    Began working on Python wrapper for DrChrono REST api

---
+++ drchrono/api.py
@@ -0,0 +1,71 @@
+import requests
+import urllib
+
+
+class ApiError(Exception):
+    pass
+
+class DrChrono(object):
+    base_url = 'https://drchrono.com'
+
+    def __init__(self, access_token):
+        self.access_token = access_token
+        self.headers = {'Authorization': 'Bearer %s' % self.access_token}
+
+    def retrieve_single(self, endpoint):
+        response = requests.get(self.base_url + endpoint, headers=self.headers)
+        if response.status_code != 200:
+            raise ApiError(response.json())
+        return response.json()
+
+    def retreive_multi(self, endpoint, params):
+        print params
+        results = []
+        url = self.base_url + endpoint + "?" + urllib.urlencode(params)
+
+        while url:
+            response = requests.get(url, headers=self.headers)
+            data = response.json()
+            if response.status_code != 200:
+                raise ApiError(data)
+            results.extend(data['results'])
+            url = data['next']
+        return results
+
+    def get_appointments(self, **kwargs):
+        """
+        Retrieves a list of appointments.
+
+        Filtering parameters:
+            date : date : Only retrieve appointments that occur on the given date
+            date_range : date range :Retrieve appointments in a time period (inclusive). Cannot be longer than 190 days, unless it is in the past.
+            doctor : integer : Only retrieve appointments for the doctor with the given ID
+            office : integer : Only retrieve appointments for the office with the given ID
+            patient : integer : Only retrieve appointments for the patient with the given ID
+            since : timestamp : Only retrieve appointments that have been updated since a given date
+        """
+        if not kwargs.get('since') and not kwargs.get('date') and not kwargs.get('date_range'):
+            raise ValueError('Must supply at least one of `since`, `date`, or `date_range`')
+
+        return self.retreive_multi('/api/appointments', kwargs)
+
+    def get_appointment(self, id):
+
+        return self.retrieve_single('/api/appointments/%d' % id)
+
+    def get_patients_summary(self, **kwargs):
+        """Retrives a list of patients
+
+        Filtering parameters:
+            date_of_birth : date : Limit to patients born on a given date
+            doctor : integer : List patients for the doctor with the given ID
+            email : string:
+            first_name : string : Case-insensitive and includes partial matches
+            since : timestamp : Only retrieve patients that have been updated since a given date
+
+        """
+        return self.retreive_multi('/api/patients', kwargs)
+
+    def get_patient(self, id):
+        return self.retrieve_single('/api/patients/%d' % id)
+
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,8 +1,11 @@
+import datetime as dt
+
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 from social_auth_drchrono.mixins import LoginRequiredMixin
 
+from .api import DrChrono
 from .utils import get_user_access_token
 
 class LandingPageView(TemplateView):
@@ -16,7 +19,15 @@ class LandingPageView(TemplateView):
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard.html'
 
+    def get_appointments(self):
+        api = DrChrono(get_user_access_token(self.request.user))
+        appointments = api.get_appointments(date=dt.date.today().isoformat())
+        for a in appointments:
+            a['patient'] = api.get_patient(a['patient'])
+        return appointments
+
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         context['access_token'] = get_user_access_token(self.request.user)
+        context['appointments'] = self.get_appointments()
         return context
